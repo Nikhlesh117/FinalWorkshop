e507cadb43683e84e77ea8d3bc9a79b669805d3a
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-01-23 09:57:36 +0530

    Final Workshop

---
+++ WhilePattern.sh
@@ -0,0 +1,28 @@
+i=0
+while [	$i -le 10 ]
+do
+	i=$(($i+1))
+	case $i in
+ 	 1)
+	 echo "1"
+	 ;;
+	 2)
+	 echo "2"
+	 ;;
+	 3)
+	 echo "3"
+	 ;;
+	 5)
+	 echo "5"
+	 ;;
+	 7)
+	 echo "7"
+	 ;;
+	 9)
+	 echo "9"
+	 ;;
+	 10)
+	 echo "10"
+	 ;;
+	esac
+done
